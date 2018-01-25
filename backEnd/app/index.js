ede95379d120db4235201f04e4037db6e68eed1b
Felipe Santa <juan.santa@nebulae.com.co>
2018-01-25 16:40:09 -0500

    stable version

--- backEnd/app/index.js
+++ backEnd/app/index.js
@@ -36,13 +36,15 @@ app.use((request, response, next) => {
   // Website you wish to allow to connect
   response.setHeader('Access-Control-Allow-Origin', '*');
   // Request methods you wish to allow
-  response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  response.setHeader('Access-Control-Allow-Methods', 'HEAD, GET, POST, OPTIONS, PUT, PATCH, DELETE');
   // Request headers you wish to allow
-  response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  response.setHeader('Access-Control-Allow-Headers', 'Origin, X-Auth-Token, X-Requested-With, content-type, Accept');
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
   response.setHeader('Access-Control-Allow-Credentials', true);
   // Pass to next layer of middleware
+  console.log("configurando el response");
+  console.log(response.getHeaders());
   next();
 });
 
